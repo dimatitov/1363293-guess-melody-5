19e430859b6be59641833231e1db05fbbed8a79e
Dmitruy Titov <titovdima95@gmail.com>
2020-10-12 20:33:21 +0500

    add react-route and created a routing of pages and also added the pages themselves

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import WelcomeScreen from "../WelcomeScreen/welcome-screen";
+import Login from "../Login/login";
+import QuestionArtist from "../QuestionArtist/question-artist";
+import QuestionGenre from "../QuestionGenre/question-genre";
+import ResultSuccess from "../ResultSuccess/result-success";
+import FailTries from "../FailTries/fail-tries";
 
 const App = ({errorCounter}) => {
   return (
-    <WelcomeScreen errorCounter={errorCounter}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={`/`}>
+          <WelcomeScreen errorCounter={errorCounter}/>
+        </Route>
+        <Route exact path={`/login`}>
+          <Login />
+        </Route>
+        <Route exact path={`/dev-artist`}>
+          <QuestionArtist />
+        </Route>
+        <Route exact path={`/dev-genre`}>
+          <QuestionGenre />
+        </Route>
+        <Route exact path={`/result`}>
+          <ResultSuccess />
+        </Route>
+        <Route exact path={`/lose`}>
+          <FailTries />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
